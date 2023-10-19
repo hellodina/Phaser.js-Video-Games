1539d524027f166071bd0a4cbae63effb2e655b5
helloDina <59027135+hellodina@users.noreply.github.com>
2023-10-18 17:53:56 -0700

    Create startScene.js

---
+++ Business Outfitted Bob/startScene.js
@@ -0,0 +1,19 @@
+class StartScene extends Phaser.Scene {
+  constructor() {
+    super({ key: 'StartScene' })
+  }
+
+  preload() {
+    this.load.image('start', 'https://content.codecademy.com/courses/learn-phaser/BOB/Start%20screen.png');
+  }
+
+  create() {
+    const screen = this.add.image(0, 0, 'start').setOrigin(0);
+    
+    // on keypress any, transition to GameScene
+    this.input.keyboard.on('keydown', () => {
+      this.scene.stop('StartScene');
+      this.scene.start('GameScene');
+    });
+  }
+}
