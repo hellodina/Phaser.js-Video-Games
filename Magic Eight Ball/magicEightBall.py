715af7ef18f9ed649ecea3514bb4585c6f924445
helloDina <59027135+hellodina@users.noreply.github.com>
2023-10-18 17:45:12 -0700

    Create magicEightBall.py

---
+++ Magic Eight Ball/magicEightBall.py
@@ -0,0 +1,41 @@
+let userName = '';
+userName ? console.log(`Hello, ${userName} !`) : console.log('Hello!');
+
+let userQuestion = 'Will I meet the love of my life?';
+console.log(userQuestion);
+
+let randomNumber = Math.floor(Math.random()*7);
+let eightBall ='';
+
+switch(randomNumber){
+  case 0: eightBall='It is certain.';
+  break;
+
+  case 1: eightBall='It is decidedly so.';
+  break
+
+  case 2: eightBall='Reply hazy try again.';
+  break;
+
+  case 3: eightBall= 'Cannot predict now.';
+  break;
+
+  case 4: eightBall= 'Do not count on it.';
+  break;
+
+  case 5: eightBall= 'My sources say no.';
+  break;
+
+  case 6: eightBall= 'Outlook not so good.';
+  break;
+
+  case 7: eightBall= 'Signs point to yes.';
+  break;
+
+  default: console.log('Out of order, try again.');
+  break;
+}
+
+console.log(eightBall);
+
+
